499582385fff11a357fbc9e35d2ce88257ee92ff
orky161 <orky161@gmail.com>
2021-02-13 20:15:45 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:12-alpine
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install --production --silent
+
+COPY . .
+
+EXPOSE 9000
+CMD [ "node", "src/server.js" ]
---
+++ src/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config("./env");
+
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 9000
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get("/ping", (req, res) => {
+    res.status(200).json({msg: "ping"})
+})
+
+app.get("/env", async (req, res) => {
+    res.status(200).json(process.env)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
