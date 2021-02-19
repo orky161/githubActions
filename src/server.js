e604faea373441f80ed9601c85c63734c2628b6d
orky161 <orky161@gmail.com>
2021-02-19 18:12:25 +0200

    pingo 2

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.get("/ping", (req, res) => {
-    res.status(200).json({msg: "pingo"})
+    res.status(200).json({msg: "pingo2"})
 })
 
 app.get("/env", async (req, res) => {
